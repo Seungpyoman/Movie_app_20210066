50f8b89c7100a65a16f07a13d99f882c0e54c899
Paul Lee <paul117@naver.com>
2022-05-19 04:09:26 +0900

    6장까지 완성된 영화앱

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
+import axios from "axios";
+import React from "react";
+import Movie from "./Movie";
 
-function App() {
-  return <div>우리 친구들 잘 하고 있어요!!</div>
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating')
+    this.setState({movies, isLoading: false});
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+            <div class="loader">
+              <span class="loader__text">Loading</span>
+            </div>
+          ) : (
+            <div class="movies">
+             {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} 
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
